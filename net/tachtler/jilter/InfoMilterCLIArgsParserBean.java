2b9bcf237adf540bf5120c725dbec16dcd7d4302
Klaus Tachtler <klaus@tachtler.net>
2022-04-19 21:59:55 +0200

    Update InfoMilterCLIArgsParserBean.java
    2022-04-19 - Vulnerability - Update jar-File to actual log4j2 version.
    Main changes are:
     - Update log4j2 to version 2.17.2 (CVE-2021-44832).

--- net/tachtler/jilter/InfoMilterCLIArgsParserBean.java
+++ net/tachtler/jilter/InfoMilterCLIArgsParserBean.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2018 Klaus Tachtler. All Rights Reserved.
+ * Copyright (c) 2022 Klaus Tachtler. All Rights Reserved.
  * Klaus Tachtler. <klaus@tachtler.net>
  * http://www.tachtler.net
  */
@@ -30,7 +30,7 @@ import org.apache.logging.log4j.Logger;
  *         implied. See the License for the specific language governing
  *         permissions and limitations under the License..
  * 
- *         Copyright (c) 2018 by Klaus Tachtler.
+ *         Copyright (c) 2022 by Klaus Tachtler.
  ******************************************************************************/
 public class InfoMilterCLIArgsParserBean {
 
